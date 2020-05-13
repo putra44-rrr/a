773b21474ad585df68f25908ce60ac38d292b87f
putra44-rrr <62072422+putra44-rrr@users.noreply.github.com>
2020-05-13 23:46:24 +0700

    Update function.php

--- function.php
+++ function.php
@@ -56,7 +56,7 @@ function save($filename, $content)
 function nama()
     {
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "http://ninjaname.horseridersupply.com/dutch_name.php");
+    curl_setopt($ch, CURLOPT_URL, "http://ninjaname.horseridersupply.com/indonesian_name.php");
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
