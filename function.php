b1f64af96be2002a3ca301be87810a376694aabf
putra44-rrr <62072422+putra44-rrr@users.noreply.github.com>
2020-06-15 16:59:51 +0700

    Update function.php

--- function.php
+++ function.php
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.30.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -7.254693,112.754554";
+$header[] = "X-Location: -6.925846,107.618357";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
